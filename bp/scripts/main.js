1f50a1d786af5790b039c9324db9b83d31041080
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-01 12:10:52 -0400

    update script to play sound

--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -3,14 +3,13 @@ import { world } from "mojang-minecraft";
 var overworld = world.getDimension("overworld");
 // overworld.runCommand()
 
-world.events.beforeItemUse.subscribe(function(e) {
+world.events.beforeItemUse.subscribe(function (e) {
   if (e.item.id === "adamraichu:sniper") {
-    overworld.runCommand("tell @a loud noise");
+    overworld.runCommand(`playsound guns.sniper.fire ${e.source.name} ~ ~ ~ 10`);
     var ent = e.source.getEntitiesFromViewVector();
     if (ent.length > 0) {
       var h = ent[0].getComponent("minecraft:health");
       h.setCurrent(h.current - 15);
     }
-    // (recoil) execute as user tp @s ^ ^ ^-1
   }
 });
