b4532eef016022e37e0572a9e0d89b41955bada7
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-01 12:41:15 -0400

    prevent spamming of fire button

--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -1,15 +1,16 @@
 import { world } from "mojang-minecraft";
 
 var overworld = world.getDimension("overworld");
-// overworld.runCommand()
 
 world.events.beforeItemUse.subscribe(function (e) {
-  if (e.item.id === "adamraichu:sniper") {
+  if (e.item.id === "adamraichu:sniper" && e.source.getItemCooldown("guns") === 0) {
     overworld.runCommand(`execute @a[name=${e.source.name}] ~ ~ ~ playsound guns.sniper.fire @s ~ ~ ~ 10`);
     var ent = e.source.getEntitiesFromViewVector();
     if (ent.length > 0) {
       var h = ent[0].getComponent("minecraft:health");
       h.setCurrent(h.current - 15);
     }
+  } else {
+    overworld.runCommand(`tell @a[name=${e.source.name}] §rStill reloading...`)
   }
 });
