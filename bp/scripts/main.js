cf4c21fdc22bdd0b07daa4b28163d2f76eef04b4
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-01 13:48:27 -0400

    change command source

--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -4,13 +4,13 @@ var overworld = world.getDimension("overworld");
 
 world.events.beforeItemUse.subscribe(function (e) {
   if (e.item.id === "adamraichu:sniper" && e.source.getItemCooldown("guns") === 0) {
-    overworld.runCommand(`execute @a[name=${e.source.name}] ~ ~ ~ playsound guns.sniper.fire @s ~ ~ ~ 10`);
+    e.source.runCommand(`playsound guns.sniper.fire @s ~ ~ ~ 10`);
     var ent = e.source.getEntitiesFromViewVector();
     if (ent.length > 0) {
       var h = ent[0].getComponent("minecraft:health");
       h.setCurrent(h.current - 15);
     }
   } else {
-    overworld.runCommand(`tell @a[name=${e.source.name}] §rStill reloading...`)
+    e.source.runCommand(`title @s actionbar Still reloading...`)
   }
 });
