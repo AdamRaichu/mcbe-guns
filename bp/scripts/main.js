b2a12af2a8dbd6c54ad4ca7bf8462db0f274fa95
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-03 20:18:40 -0400

    fix playsound command

--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -5,7 +5,7 @@ var overworld = world.getDimension("overworld");
 world.events.beforeItemUse.subscribe(function (e) {
   if (e.item.id === "adamraichu:sniper") {
     if (e.source.getItemCooldown("guns") === 0) {
-      e.source.runCommand(`playsound guns.sniper.fire @s ~ ~ ~ 10`);
+      e.source.runCommand(`playsound guns.sniper.fire @a[r=160] ~ ~ ~ 10`);
       var ent = e.source.getEntitiesFromViewVector();
       if (ent.length > 0) {
         var h = ent[0].getComponent("minecraft:health");
