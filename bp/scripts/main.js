185dcc7ad881b165ee358acf8e80d2ce835277eb
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-03 20:17:01 -0400

    fix still reloading from appearing for every item use

--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -3,14 +3,16 @@ import { world } from "mojang-minecraft";
 var overworld = world.getDimension("overworld");
 
 world.events.beforeItemUse.subscribe(function (e) {
-  if (e.item.id === "adamraichu:sniper" && e.source.getItemCooldown("guns") === 0) {
-    e.source.runCommand(`playsound guns.sniper.fire @s ~ ~ ~ 10`);
-    var ent = e.source.getEntitiesFromViewVector();
-    if (ent.length > 0) {
-      var h = ent[0].getComponent("minecraft:health");
-      h.setCurrent(h.current - 15);
+  if (e.item.id === "adamraichu:sniper") {
+    if (e.source.getItemCooldown("guns") === 0) {
+      e.source.runCommand(`playsound guns.sniper.fire @s ~ ~ ~ 10`);
+      var ent = e.source.getEntitiesFromViewVector();
+      if (ent.length > 0) {
+        var h = ent[0].getComponent("minecraft:health");
+        h.setCurrent(h.current - 15);
+      }
+    } else {
+      e.source.runCommand(`title @s actionbar Still reloading...`)
     }
-  } else {
-    e.source.runCommand(`title @s actionbar Still reloading...`)
   }
 });
