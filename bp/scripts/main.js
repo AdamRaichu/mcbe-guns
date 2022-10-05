272c38d06ea14d63eece4137d00ac455e428c7ea
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-04 20:09:08 -0400

    update main.js

--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -1,10 +1,29 @@
-import { world } from "mojang-minecraft";
+import { world, MinecraftEffectTypes } from "mojang-minecraft";
 import { Test as test } from "mojang-gametest";
 
 var overworld = world.getDimension("overworld");
+var joinedPlayers = [];
+
+
+world.events.playerJoin.subscribe(function (e) {
+  joinedPlayers.push(e.player);
+});
+
+world.events.tick.subscribe(function (e) {
+  for (var c = 0; c < joinedPlayers.length; c++) {
+    if (typeof joinedPlayers[c] === "undefined") {
+      overworld.runCommand("tell @a undefined was found in playerlist");
+      return
+    }
+    var inv = joinedPlayers[c].getComponent("minecraft:inventory").container
+    if (inv.getItem(joinedPlayers[c].selectedSlot).id === "guns:sniper" && joinedPlayers[c].isSneaking) {
+      joinedPlayers[c].addEffect(MinecraftEffectTypes.slowness, 5, 255, false)
+    }
+  }
+});
 
 world.events.beforeItemUse.subscribe(function (e) {
-  if (e.item.id === "adamraichu:sniper") {
+  if (e.item.id === "guns:sniper") {
     if (e.source.getItemCooldown("guns") === 0) {
       e.source.runCommand(`playsound guns.sniper.fire @a[r=160] ~ ~ ~ 10`);
       var ent = e.source.getEntitiesFromViewVector();
@@ -15,12 +34,6 @@ world.events.beforeItemUse.subscribe(function (e) {
       }
     } else {
       e.source.runCommand(`title @s actionbar Still reloading...`)
-    }
-  } else if (e.item.id === "adamraichu:scope") {
-    if (e.source.hasTag("_guns_001")) {
-      e.source.runCommand("tag @s remove _guns__001");
-    } else {
-      e.source.runCommand("tag @s add _guns__001");
     }
   }
 });
