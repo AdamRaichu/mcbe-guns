3c2ce7909301689a1d272712e070f9e9ad954c6b
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-10-06 09:07:03 -0400

    switch to iterator for scoping

--- bp/scripts/main.js
+++ bp/scripts/main.js
@@ -2,36 +2,24 @@ import { world, MinecraftEffectTypes } from "mojang-minecraft";
 import { Test as test } from "mojang-gametest";
 
 var overworld = world.getDimension("overworld");
-var joinedPlayers = [];
-
-
-world.events.playerJoin.subscribe(function (e) {
-  joinedPlayers.push(e.player);
-});
-
-world.events.playerLeave.subscribe(function (e) {
-  joinedPlayers = joinedPlayers.filter(function (element) {
-    return element !== undefined;
-  });
-})
 
 world.events.tick.subscribe(function (e) {
-  for (var c = 0; c < joinedPlayers.length; c++) {
-    if (typeof joinedPlayers[c] === "undefined") {
-      overworld.runCommand("tell @a undefined was found in playerlist");
-      return
-    }
-    var inv = joinedPlayers[c].getComponent("minecraft:inventory").container
-    var selected = inv.getItem(joinedPlayers[c].selectedSlot)
+  // For scoping
+  var players = world.getPlayers();
+  for (var p of players) {
+    var inv = p.getComponent("minecraft:inventory").container;
+    var selected = inv.getItem(p.selectedSlot);
     if (typeof selected !== "undefined") {
-      if (selected.id === "guns:sniper" && joinedPlayers[c].isSneaking) {
-        joinedPlayers[c].addEffect(MinecraftEffectTypes.slowness, 5, 255, false)
+      // this is in case player is holding nothing
+      if (selected.id === "guns:sniper" && p.isSneaking) {
+        p.addEffect(MinecraftEffectTypes.slowness, 5, 255, false);
       }
     }
   }
 });
 
 world.events.beforeItemUse.subscribe(function (e) {
+  // For rifle firing
   if (e.item.id === "guns:sniper") {
     if (e.source.getItemCooldown("guns") === 0) {
       e.source.runCommand(`playsound guns.sniper.fire @a[r=160] ~ ~ ~ 10`);
@@ -39,11 +27,11 @@ world.events.beforeItemUse.subscribe(function (e) {
       if (ent.length > 0) {
         var h = ent[0].getComponent("minecraft:health");
         var armor = 1;
-        ent[0].runCommand("summon snowball ~ ~.2 ~")
+        ent[0].runCommand("summon snowball ~ ~.2 ~");
         h.setCurrent(h.current - (15 * armor));
       }
     } else {
-      e.source.runCommand(`title @s actionbar Still reloading...`)
+      e.source.runCommand(`title @s actionbar Still reloading...`);
     }
   }
 });
